c8911d305390150691bf683771043e8a26e0b939
abdoshawky <abdelrhman.shawky@areebgroup.com>
2021-05-02 00:19:30 +0200

    create main screens

---
+++ screens/Line.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Line extends Component
+{
+    render() {
+        return (
+            <View>
+                <Text>Line</Text>
+            </View>
+        );
+    }
+}
+
+export default Line;
